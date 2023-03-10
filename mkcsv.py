c146ec45e02e3a6a236be949cb98327c30581a47
xaverhimmelsbach <xaver.himmelsbach@gmail.com>
2023-03-10 19:40:30 +0100

    Visualize optimal

--- mkcsv.py
+++ mkcsv.py
@@ -2,16 +2,16 @@ import os
 import csv
 
 # Set the directory that contains the subdirectories
-root_dir = "/home/xaver/Nextcloud/Semester 11/Measurements/_deduplication"
+root_dir = "/home/xaver/Nextcloud/Semester 11/Measurements/_optimal"
 
 # Open the output CSV file
-with open("deduplication.csv", "w", newline="") as csvfile:
+with open("optimal.csv", "w", newline="") as csvfile:
     # Create a CSV writer
     csvwriter = csv.writer(csvfile)
 
     # Write the header row
     csvwriter.writerow(["file_name", "file_size_original", "file_size_transcoded",
-                       "compression_ratio", "transcoding_time", "similarity"])
+                       "compression_ratio", "transcoding_time"])
 
     # Iterate over the subdirectories in the root directory
     for subdir in os.listdir(root_dir):
@@ -30,11 +30,10 @@ with open("deduplication.csv", "w", newline="") as csvfile:
         meta_path = os.path.join(root_dir, subdir, "meta.txt")
         with open(meta_path, "r") as metafile:
             meta_lines = metafile.readlines()
-            similarity = meta_lines[0].split('_')[1]
             # Convert seconds to milliseconds
             transcoding_time = round(float(meta_lines[1].strip()) * 1000)
             filename = meta_lines[2].strip().rstrip('.png')
 
         # Write a row to the CSV file
         csvwriter.writerow([filename, file_size_original, file_size_transcoded,
-                           compression_ratio, transcoding_time, similarity])
+                           compression_ratio, transcoding_time])
