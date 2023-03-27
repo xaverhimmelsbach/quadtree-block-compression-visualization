ae51f56b35d9ede421c524c06a67aa5a450db544
xaverhimmelsbach <xaver.himmelsbach@gmail.com>
2023-03-27 14:59:20 +0200

    more sorting

--- sortcsv.py
+++ sortcsv.py
@@ -1,11 +1,11 @@
 import csv
 
 # Define the input and output file paths
-input_file = './data/sampling.csv'
-output_file = './data/sampling_sorted.csv'
+input_file = './data/deduplication.csv'
+output_file = './data/deduplication_sorted.csv'
 
 # Define the columns to sort by
-sort_columns = ['file_name', 'downsampling_algorithm', 'upsampling_algorithm']
+sort_columns = ['file_name', 'similarity']
 
 # Read the input file and store the data in a list
 with open(input_file, 'r') as f:
@@ -13,7 +13,7 @@ with open(input_file, 'r') as f:
     data = list(reader)
 
 # Sort the data by the specified columns
-data.sort(key=lambda x: (x[sort_columns[0]], x[sort_columns[1]], x[sort_columns[2]]))
+data.sort(key=lambda x: (x[sort_columns[0]], x[sort_columns[1]]))
 
 # Write the sorted data to the output file
 with open(output_file, 'x', newline='') as f:
